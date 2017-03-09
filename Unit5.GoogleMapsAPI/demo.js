a4bdd3eac2e9ab7cb8ca4821d47f50ce13568c3c
AlphaRoe-HauNguyen <hau.nguyen@alpharoe.com>
2017-03-09 22:57:36 +0700

    Prepare Unit 5: Google Maps APIs

---
+++ Unit5.GoogleMapsAPI/demo.js
@@ -0,0 +1,14 @@
+var map;
+
+function initMap() {
+    $(function() {
+        map = new google.maps.Map($('.gmap')[0], {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8
+        });
+    });
+}
+
+
+
+//AIzaSyDia01skabytp_FX9ErPReOhOtCBxDn5Dw
