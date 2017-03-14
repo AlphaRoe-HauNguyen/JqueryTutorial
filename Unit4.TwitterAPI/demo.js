b7f5e14b728fdbef41cf16889a1b9ee07963da63
Hau Nguyen <Hau Nguyen>
2017-03-14 17:30:28 +0700

    ImproveEx4

--- Unit4.TwitterAPI/demo.js
+++ Unit4.TwitterAPI/demo.js
@@ -40,9 +40,44 @@ function MyTwitterApp(apiKey, apiSecret) {
 	return $this;
 }
 
+var randomString = function(length) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for(var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
 $(function(){
-	$('#do-action').click(function(){
-		var app = new MyTwitterApp('poNNBhnBXE1ewQI8nWA8Yrhxg','Kxz7UQWzzrV67a34LR6is0a0C8jpODLgFtQue3GTL1JHLcgjSW');
-		app.getAccessToken();
-	});
+	// $('#do-action').click(function(){
+	// 	var app = new MyTwitterApp('poNNBhnBXE1ewQI8nWA8Yrhxg','Kxz7UQWzzrV67a34LR6is0a0C8jpODLgFtQue3GTL1JHLcgjSW');
+	// 	app.getAccessToken();
+	// });
+	$.ajax({
+			url:"https://api.twitter.com/oauth/request_token",
+			method: 'post',
+			contentType : "application/x-www-form-urlencoded;charset=UTF-8",
+			crossDomain: true,
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("Authorization", "OAuth oauth_callback='http%3A%2F%2Flocalhost%2Fjq%2Findex.html'");
+				xhr.setRequestHeader('Access-Control-Allow-Origin','*');
+			},
+			success: function(data) {
+				console.info(data);
+			},
+			error: function(err) {
+				console.log(err);
+			}
+		});
 });
+var time = new Date().getTime();
+var nonce = randomString(32);
+
+var oauth_callback = 'oauth_callback="http%3A%2F%2Flocalhost%2Fjq%2Findex.html"';
+var oauth_consumer_key = 'oauth_consumer_key="poNNBhnBXE1ewQI8nWA8Yrhxg"';
+var oauth_nonce = 'oauth_nonce=""';
+var oauth_signature = 'oauth_signature=""';
+var oauth_signature_method = 'oauth_signature_method="HMAC-SHA1"';
+var oauth_timestamp = 'oauth_timestamp="'+time+'"';
+var oauth_version = 'oauth_version="1.0"'
